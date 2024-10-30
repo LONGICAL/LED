b3f48df3fe91c7b6b817225104fc6ba6613f08f0
LONGICAL <763113963@qq.com>
2024-10-30 22:03:33 +0800

    Update freertos.c

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -175,6 +175,7 @@ void StartledTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
+    
     HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_SET);
     osDelay(100);
     HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_RESET);
